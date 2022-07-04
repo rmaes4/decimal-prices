4773c94ec3afb020ca9ffa4efa5ac16462f1b11c
Rob Maes <remilmaes@gmail.com>
2022-07-04 14:47:15 -0500

    update getVar/setVar to new getVarc/setVarc methods. prevent typing more than one decimal.

--- src/main/java/com/decimalprices/DecimalPrices.java
+++ src/main/java/com/decimalprices/DecimalPrices.java
@@ -1,39 +1,37 @@
 package com.decimalprices;
 
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
+import net.runelite.api.Client;
 import net.runelite.client.input.KeyManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import javax.inject.Inject;
+
 @Slf4j
 @PluginDescriptor(
-	name = "Decimal Prices"
+  name = "Decimal Prices"
 )
-public class DecimalPrices extends Plugin
-{
-	@Inject
-	private Client client;
+public class DecimalPrices extends Plugin {
+  @Inject
+  private Client client;
 
-	@Inject
-	private KeyManager keyManager;
+  @Inject
+  private KeyManager keyManager;
 
-	@Inject
-	private DecimalPricesKeyListener inputListener;
+  @Inject
+  private DecimalPricesKeyListener inputListener;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		keyManager.registerKeyListener(inputListener);
-		log.info("Decimal prices started!");
-	}
+  @Override
+  protected void startUp() throws Exception {
+    keyManager.registerKeyListener(inputListener);
+    log.info("Decimal prices started!");
+  }
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		keyManager.unregisterKeyListener(inputListener);
-		log.info("Decimal prices stopped!");
-	}
+  @Override
+  protected void shutDown() throws Exception {
+    keyManager.unregisterKeyListener(inputListener);
+    log.info("Decimal prices stopped!");
+  }
 
 }
